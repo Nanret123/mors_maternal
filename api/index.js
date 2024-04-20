e9580b9377049e2a9e24763a6424ac3f387093de
nanret_dev <nanretgungshik@gmail.com>
2024-04-20 21:08:52 +0100

    fixed backend routes

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@ app.use(
     credentials: true,
   })
 );
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/doctors', doctorRoutes);
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/reviews', reviewRoutes);
+app.use('/auth', authRoutes);
+app.use('/doctors', doctorRoutes);
+app.use('/users', userRoutes);
+app.use('/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
   res.send('App is working');
