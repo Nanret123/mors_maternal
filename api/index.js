3993c8f5f7449dc91cbb9b1706ef1ceecc1b7b57
nanret_dev <nanretgungshik@gmail.com>
2024-04-20 20:34:46 +0100

    fixed cors error

--- api/index.js
+++ api/index.js
@@ -13,11 +13,17 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://mors-maternal-frontend.vercel.app'],
+    methods: ['POST', 'PUT', 'GET', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/doctors',doctorRoutes);
-app.use('/api/v1/users',userRoutes);
-app.use('/api/v1/reviews',reviewRoutes);
+app.use('/api/v1/doctors', doctorRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
   res.send('App is working');
